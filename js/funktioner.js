189bc1bde96e56c138109f6a3878f18f40ae569a
mediagymnasiet-webbutveckling <55544791+mediagymnasiet-webbutveckling@users.noreply.github.com>
2019-12-06 08:46:24 +0100

    changes

--- js/funktioner.js
+++ js/funktioner.js
@@ -1,16 +1,10 @@
 //Funktionsdeklaration
 
-
-
 //Funktionsutryck (function expression)
-
 
 //Hoisting - bra att veta
 
-
-
 //Bonus: Arrow funcitons
-const fahhrenheit = degrees => degrees * 12;
 
 //Uppgift: Gör om till en arrow function
 /* function add(a, b = 3) {
@@ -18,15 +12,9 @@ const fahhrenheit = degrees => degrees * 12;
     return total;
 } */
 
-
 //IIFE - Immediately invoked function
 
-
-
 //Metod - en funktion som lever i ett objekt
-
-
-
 
 //Callback functions
 //setTimout
