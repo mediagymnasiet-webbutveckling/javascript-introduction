e65760f57cc7c01abe05656746674cc5d52dc268
mediagymnasiet-webbutveckling <55544791+mediagymnasiet-webbutveckling@users.noreply.github.com>
2019-12-06 14:25:56 +0100

    lektion6dec

--- js/datatyper.js
+++ js/datatyper.js
@@ -1,6 +1,57 @@
+/* JS har dynamisk typning vilket betyder att 
+dataypen tilldelas automatiskt till variablerna */
 // SNOBUSN
-//Dynamisk typning
 
+//1. String
 
+const name = "Sandra";
+const city = "Stockholm";
 
+//console.log("Jag heter " + name + " och bor i " + city);
 
+//Template strings ES6 - backticks - smidigt och bra!
+console.log(`Jag heter ${name}  och bor i ${city}`);
+
+// 2. Number - double, vilket gör att du även kan använda decimaler
+
+let age = 99.00023443224;
+age++;
+console.log(age);
+
+let test = 5 + "5";
+console.log(test); //Blir 55
+
+console.log(typeof name); //Typeof kollar vilken datatyp det är!
+
+//3. Objekt
+
+var person = {
+  name: "Sandra",
+  age: 20,
+  hobbies: ["teaching", "movies"]
+};
+console.log(person.name);
+
+// 4. Boolean - true/false
+
+let friday = true;
+
+// == vs === Best practice - använd alltid tre!
+// === gör även check på datayp!
+let a = 2;
+let b = 2;
+if (a === b) {
+  console.log("a och b är samma även vad gäller datatyp");
+}
+
+//5. Undefined - om en variabel inte initierats med ett värde!
+let number;
+console.log(number); //Ge undefined
+
+//6. Null
+//Nästan som undefinied fast om man vill sätta det till "inget" i efterhand
+
+let age = 99;
+age = null; //Sätter variabeln till inget
+
+//7. Symbol //Ny i ES6 . Vi kommer inte använda den, så du behöver inte lära dig den
--- js/variabler.js
+++ js/variabler.js
@@ -1,20 +1,34 @@
 //1. var, let och const
-
-let name = "Sandra";
-
 //2. strict mode
+"use strict"; //Bra att ha i början av ett script för att skriva säker kod!
 
-//3. eslint-disable
+var name = "Sandra"; //ES1  - gammal kvarleva
+console.log(name);
 
-//4. Uppgift var vs let/const
-/*function foo() {
-    var par = 1; 
-    if (par >= 0) {
-        let bar = 2;
-        console.log(par); // prints 1
-        console.log(bar); // prints 2
-    }
+age = 20;
+console.log(age);
+
+const name = "Sandra"; //ES6 - använd detta!
+//name = "SSSandra"; //går ej att ändra
+//console.log(name);
+
+let age = 20;
+age = 21;
+console.log(age);
+
+// var - functionscope
+// let, const - blockscope
+
+//3. Uppgift var vs let/const - vad händer om du
+// deklarerar med let inne i funktionen till skillnad från var?
+function foo() {
+  let par = 1;
+  if (par >= 0) {
+    let bar = 2;
     console.log(par); // prints 1
     console.log(bar); // prints 2
+  }
+  console.log(par); // prints 1
+  console.log(bar); // prints 2
 }
-foo();*/
+foo();
